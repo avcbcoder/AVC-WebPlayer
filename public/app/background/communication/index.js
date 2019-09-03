4a414217fe321191c810c61b405ff1ae3eea593e
Ankit verma <ankitvermamat@gmail.com>
2019-09-03 08:50:26 +0530

    search using http req in background.js

--- public/app/background/communication/index.js
+++ public/app/background/communication/index.js
@@ -1,84 +1,72 @@
 /*global chrome*/
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.type === "toggle") {
-       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-          for (let i = 0; i < tabs.length; i++) {
-             var activeTab = tabs[i];
-             chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action", "tabs": tabs });
-          }
-       });
-       chrome.tabs.query({}, function (tabs) {
-          for (let i = 0; i < tabs.length; i++)
-             if (tabs[i].url.includes('//open.spotify.com')) {
-                chrome.tabs.executeScript(tabs[i].id, { file: "app/background-script/spotify.js" })
-             }
-       });
-    }
- });
- 
- chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.type === "media") {
-       chrome.tabs.query({}, function (tabs) {
-          for (let i = 0; i < tabs.length; i++) {
-             const tab = tabs[i];
-             if (tab.url.includes('//open.spotify.com')) {
-                chrome.tabs.executeScript(tab.id, { code: `spotifyPageElements.${request.options.type}.click()` });
-             }
-          }
-       });
-    }
- });
- 
- chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.type === "mini-mode") {
-       chrome.tabs.create({ url: request.options.url, active: false, index: 0 }, function (tab) {
-          // chrome.tabs.executeScript(tab.id, { file: "app/youtube-script.js" })
-       });
-    }
-    if (request.type === "start-mini-mode") {
-       chrome.tabs.query({}, function (tabs) {
-          for (let i = 0; i < tabs.length; i++) {
-             const tab = tabs[i];
-             if (tab.url.includes('//www.youtube.com')) {
-                chrome.tabs.executeScript(tab.id, { file: "app/youtube-script.js" })
-             }
-          }
-       });
-    }
- });
- 
- chrome.tabs.onCreated.addListener(function (tab) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-       chrome.extension.getBackgroundPage().console.log('tab created');
-       chrome.storage.local.set({ 'url': tabs[0].url }, function () { });
-    })
- });
- 
- chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-       chrome.storage.local.set({ 'url': tabs[0].url }, function () { });
-    })
- });
- 
- 
- // -------------------------- CODE CLEANUP
- 
- chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.type === "spotify") {
-       chrome.tabs.query({}, function (tabs) { // DO NOT SEND TO ALL TABS
-          for (let i = 0; i < tabs.length; i++) {
-             chrome.tabs.sendMessage(
-                tabs[i].id,
-                {
-                   type: request.type,
-                   method: request.method,
-                   data: request.data
-                }
-             );
-          }
-       });
-    }
- });
- 
- 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.type === "toggle") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      for (let i = 0; i < tabs.length; i++) {
+        var activeTab = tabs[i];
+        chrome.tabs.sendMessage(activeTab.id, {
+          message: "clicked_browser_action",
+          tabs: tabs
+        });
+      }
+    });
+    chrome.tabs.query({}, function(tabs) {
+      for (let i = 0; i < tabs.length; i++)
+        if (tabs[i].url.includes("//open.spotify.com")) {
+          chrome.tabs.executeScript(tabs[i].id, {
+            file: "app/background-script/spotify.js"
+          });
+        }
+    });
+  }
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.type === "media") {
+    chrome.tabs.query({}, function(tabs) {
+      for (let i = 0; i < tabs.length; i++) {
+        const tab = tabs[i];
+        if (tab.url.includes("//open.spotify.com")) {
+          chrome.tabs.executeScript(tab.id, {
+            code: `spotifyPageElements.${request.options.type}.click()`
+          });
+        }
+      }
+    });
+  }
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.type === "mini-mode") {
+    chrome.tabs.create(
+      { url: request.options.url, active: false, index: 0 },
+      function(tab) {
+        // chrome.tabs.executeScript(tab.id, { file: "app/youtube-script.js" })
+      }
+    );
+  }
+  if (request.type === "start-mini-mode") {
+    chrome.tabs.query({}, function(tabs) {
+      for (let i = 0; i < tabs.length; i++) {
+        const tab = tabs[i];
+        if (tab.url.includes("//www.youtube.com")) {
+          chrome.tabs.executeScript(tab.id, { file: "app/youtube-script.js" });
+        }
+      }
+    });
+  }
+});
+
+chrome.tabs.onCreated.addListener(function(tab) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.extension.getBackgroundPage().console.log("tab created");
+    chrome.storage.local.set({ url: tabs[0].url }, function() {});
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.storage.local.set({ url: tabs[0].url }, function() {});
+  });
+});
--- public/app/background/network/index.js
+++ public/app/background/network/index.js
@@ -7,6 +7,10 @@ const STORE_VAR = {
   MODE: "mode"
 };
 
+const console = () => {
+  //   chrome.extension.getBackgroundPage().console.log(arguments.join(" | "));
+};
+
 const render = () => {
   chrome.tabs.query({}, function(tabs) {
     // DO NOT SEND TO ALL TABS
@@ -19,82 +23,121 @@ const render = () => {
 };
 
 const replace = (str, a, b) => {
-    return str.split(a).join(b)
-}
+  return str.split(a).join(b);
+};
 
 const filter = (str, space) => {
-    let track = []
-    for (let i = 0; i < str.length; i++)
-        if (str[i] === '(')
-            break;
-        else if (str[i] === ' ')
-            track.push(space)
-        else
-            track.push((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= '0' && str[i] <= '9') ? str[i] : '')
-    return track.join('')
-}
+  let track = [];
+  for (let i = 0; i < str.length; i++)
+    if (str[i] === "(") break;
+    else if (str[i] === " ") track.push(space);
+    else
+      track.push(
+        (str[i] >= "a" && str[i] <= "z") || (str[i] >= "0" && str[i] <= "9")
+          ? str[i]
+          : ""
+      );
+  return track.join("");
+};
+
+const getYoutubeVideos = (searchString, successCallback) => {
+  chrome.extension.getBackgroundPage().console.log("searching in youtube");
+  const searchURL = `https://www.youtube.com/results?search_query=${searchString}&page=&utm_source=opensearch`;
+  $.ajax({
+    url: searchURL,
+    success: function(htmlData) {
+      const videos = [];
+      const doc = new DOMParser().parseFromString(htmlData, "text/html");
+      const videoList = document.body.getElementsByTagName(
+        "ytd-video-renderer"
+      );
+      chrome.extension
+        .getBackgroundPage()
+        .console.log("Youtube res", doc, doc.body,videoList);
+      videoList.forEach(video => {
+        const videoLink = video.getElementsByTagName("a")[0].href;
+        videos.push(videoLink);
+      });
+      successCallback(videos);
+    }
+  });
+};
 
 const getFandomLyrics = (track, artist, successCallback, failureCallback) => {
-    track = filter(track.toLowerCase(), '_')
-    artist = filter(artist.toLowerCase(), '_')
-    const fandomURL = `https://lyrics.fandom.com/wiki/${artist}:${track}`
-    $.ajax({
-        url: fandomURL, success: function (htmlData) {
-            const doc = new DOMParser().parseFromString(htmlData, "text/html");
-            const lyricBox = doc.body.getElementsByClassName('lyricbox')[0]
-            let lyrics = []
-            const allText = (ele) => {
-                ele.childNodes.forEach((val, idx) => {
-                    if (val.nodeName.toString().toLowerCase() === '#text')
-                        lyrics.push(val ? val.data ? replace(val.data, '\n', '') : '' : '')
-                    else if (val.nodeName.toString().toLowerCase() === 'br')
-                        lyrics.push('\n')
-                    else allText(val)
-                });
-            }
-            allText(lyricBox)
-            successCallback(lyrics.join(''))
-        },
-        statusCode: {
-            404: function () { failureCallback(); }
-        },
-        error: function () { failureCallback() },
-        fail: function () { failureCallback() },
-    });
-}
+  track = filter(track.toLowerCase(), "_");
+  artist = filter(artist.toLowerCase(), "_");
+  const fandomURL = `https://lyrics.fandom.com/wiki/${artist}:${track}`;
+  $.ajax({
+    url: fandomURL,
+    success: function(htmlData) {
+      const doc = new DOMParser().parseFromString(htmlData, "text/html");
+      const lyricBox = doc.body.getElementsByClassName("lyricbox")[0];
+      let lyrics = [];
+      const allText = ele => {
+        ele.childNodes.forEach((val, idx) => {
+          if (val.nodeName.toString().toLowerCase() === "#text")
+            lyrics.push(
+              val ? (val.data ? replace(val.data, "\n", "") : "") : ""
+            );
+          else if (val.nodeName.toString().toLowerCase() === "br")
+            lyrics.push("\n");
+          else allText(val);
+        });
+      };
+      allText(lyricBox);
+      successCallback(lyrics.join(""));
+    },
+    statusCode: {
+      404: function() {
+        failureCallback();
+      }
+    },
+    error: function() {
+      failureCallback();
+    },
+    fail: function() {
+      failureCallback();
+    }
+  });
+};
 
 const getAzLyrics = (track, artist, successCallback, failureCallback) => {
-    track = filter(track.toLowerCase(), '')
-    artist = filter(artist.toLowerCase(), '')
-    const azURL = `https://www.azlyrics.com/lyrics/${artist}/${track}.html`
-    $.ajax({
-        url: azURL,
-        success: function (htmlData) {
-            const doc = new DOMParser().parseFromString(htmlData, "text/html");
-            const ringtone = doc.body.getElementsByClassName('ringtone')[0]
-            const sibling = ringtone.parentElement.childNodes
+  track = filter(track.toLowerCase(), "");
+  artist = filter(artist.toLowerCase(), "");
+  const azURL = `https://www.azlyrics.com/lyrics/${artist}/${track}.html`;
+  $.ajax({
+    url: azURL,
+    success: function(htmlData) {
+      const doc = new DOMParser().parseFromString(htmlData, "text/html");
+      const ringtone = doc.body.getElementsByClassName("ringtone")[0];
+      const sibling = ringtone.parentElement.childNodes;
 
-            let lyricBox = document.createElement('div')
-            for (let i = 0; i < sibling.length; i++) {
-                if (sibling[i].childNodes.length > lyricBox.childNodes.length)
-                    lyricBox = sibling[i]
-            }
-            let lyrics = []
-            lyricBox.childNodes.forEach((val, idx) => {
-                if (val.nodeName.toString().toLowerCase() === '#text')
-                    lyrics.push(val ? val.data ? replace(val.data, '\n', '') : '' : '')
-                if (val.nodeName.toString().toLowerCase() === 'br')
-                    lyrics.push('\n')
-            });
-            successCallback(replace(lyrics.join(''), '\n\n\n', '\n\n'))
-        },
-        statusCode: {
-            404: function () { failureCallback(); }
-        },
-        error: function () { failureCallback() },
-        fail: function () { failureCallback() },
-    });
-}
+      let lyricBox = document.createElement("div");
+      for (let i = 0; i < sibling.length; i++) {
+        if (sibling[i].childNodes.length > lyricBox.childNodes.length)
+          lyricBox = sibling[i];
+      }
+      let lyrics = [];
+      lyricBox.childNodes.forEach((val, idx) => {
+        if (val.nodeName.toString().toLowerCase() === "#text")
+          lyrics.push(val ? (val.data ? replace(val.data, "\n", "") : "") : "");
+        if (val.nodeName.toString().toLowerCase() === "br") lyrics.push("\n");
+      });
+      successCallback(replace(lyrics.join(""), "\n\n\n", "\n\n"));
+    },
+    statusCode: {
+      404: function() {
+        failureCallback();
+      }
+    },
+    error: function() {
+      failureCallback();
+    },
+    fail: function() {
+      failureCallback();
+    }
+  });
+};
 
 const fetchApi = (storage, songDetails) => {
   const { title, artist } = songDetails;
@@ -125,7 +168,26 @@ const fetchApi = (storage, songDetails) => {
     getFandomLyrics(title, artist[i], onSuccessLyrics, onFailureLyrics);
     getAzLyrics(title, artist[i], onSuccessLyrics, onFailureLyrics);
   }
+
+  // for youtube videos
+  const searchString = title + "+" + artist.join("+");
+  getYoutubeVideos(searchString, videos => {});
+  //   const ytSearchReq = gapi.client.youtube.search.list({
+  //     part: "snippet",
+  //     type: "video",
+  //     q: encodeURIComponent(searchString).replace(/%20/g, "+"),
+  //     maxResults: 3,
+  //     order: "viewCount",
+  //     publishedAfter: "2015-01-01T00:00:00Z"
+  //   });
+  //   ytSearchReq.execute(function(response) {
+  //     console("success yt search", response, response.result);
+  //   });
 };
+
+// api key -> AIzaSyD91jOqElBJNXKvCFIfXd_VzyOXXiJuAZQ
+// gapi.client.setApiKey("AIzaSyD91jOqElBJNXKvCFIfXd_VzyOXXiJuAZQ");
+// gapi.client.load("youtube", "v3", function() {});
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.type !== "spotify") return;
@@ -148,5 +210,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     });
   });
 
-  if (request.method === "song-change") fetchApi(storage, request.data);
+  if (request.method === "song-change") {
+    // console(gapi);
+    // gapi.client.setApiKey("AIzaSyD91jOqElBJNXKvCFIfXd_VzyOXXiJuAZQ");
+    // gapi.client.load("youtube", "v3", function() {});
+    fetchApi(storage, request.data);
+  }
 });
